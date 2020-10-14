d781f44192644f5bdc104efc1e2b36725dde0c9b
lierlin <1690480192@qq.com>
2020-10-14 21:13:43 +0800

    项目初始结构创建

---
+++ shop-product/src/test/java/com/lierlin/shop/product/ShopProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lierlin.shop.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
